0266dcd55d8c377881a0168e2ddd703ea0187c6a
sar2007 <173072045+sar-valliappan@users.noreply.github.com>
2025-05-20 15:47:53 -0700

    scraping single page, selecting specified course

---
+++ assist.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import WebDriverException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = None  # Initialize driver variable outside try block
+
+try:
+    # Initialize Chrome WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # URL of the webpage
+    url = 'https://assist.org/transfer/results?year=75&institution=7&agreement=150&agreementType=from&view=agreement&viewBy=dept&viewSendingAgreements=false&viewByKey=75%2F150%2Fto%2F7%2FAllDepartments'
+    driver.get(url)
+
+    # Wait for the presence of an element specified by XPath
+    search = True
+    i = 1
+    while (search):
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, f'//awc-agreement-row[{i}]'))
+        )
+        if ("VIS 85A" in element.text) :
+            print("Element found: ", element.text)
+            search = False
+        i += 1
+    
+    # If the element is found, print its text (or handle it as needed)
+    print("Element found: ", element.text)
+
+
+except TimeoutException:
+    print("Timed out waiting for page to load or element to appear.")
+except WebDriverException as e:
+    print(f"WebDriver encountered an issue: {e}")
+finally:
+    # Ensure the driver quits no matter what
+    if driver is not None:  # Only quit if driver was succjssfully created
+        driver.quit()
